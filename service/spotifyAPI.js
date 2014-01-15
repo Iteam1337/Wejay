959923538af5ec4a7328b301be283deff87dd97b
Johan Öbrink <johan.obrink@gmail.com>
2014-01-15 20:34:33 +0100

    Stubbed out droppability

--- service/spotifyAPI.js
+++ service/spotifyAPI.js
@@ -3,13 +3,57 @@ angular.module('wejay').service('spotifyAPI',function($rootScope) {
 
   'use strict';
 
-	var spotifyAPI = {};
+	var spotifyAPI = {
+    events: {}
+  };
 
-  require(['$api/auth#Auth'], function (Auth) {
+  spotifyAPI.on = spotifyAPI.addEventListener = function(name, listener) {
+    if(!(this.events[name] && this.events[name] instanceof Array)) {
+      this.events[name] = [];
+    }
+    this.events[name].push(listener);
+  };
+
+  spotifyAPI.off = spotifyAPI.removeEventListener = function(name, listener) {
+    if(this.events[name] instanceof Array) {
+      this.events[name] = this.events[name].filter(function(l) { return l !== listener; });
+      if(!this.events[name].length) {
+        delete this.events[name];
+      }
+    }
+  };
+
+  spotifyAPI.removeEventListeners = function(name) {
+    if(name) {
+      delete this.events[name];
+    } else {
+      this.events = {};
+    }
+  };
+
+  spotifyAPI.emit = function(name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    if(this.events[name] instanceof Array) {
+      this.events[name].forEach(function(l) {
+        l.apply(null, args);
+      });
+    }
+  };
+
+  require(['$api/auth#Auth', '$api/models'], function (Auth, models) {
+    models.application.addEventListener('dropped', function() {
+      spotifyAPI.emit('dropped', models.application.dropped);
+    });
+
     spotifyAPI.auth = new Auth();
+    spotifyAPI.models = models;
 
     $rootScope.$broadcast('appReady');
   });
+
+  /*models.application.addEventListener('dropped', function() {
+   var dropped = models.application.dropped; // it contains the dropped elements
+  });*/
 
 	return spotifyAPI;
 });
---
+++ test/unit/service/playlist.js
@@ -0,0 +1,11 @@
+describe('playlist', function() {
+
+  beforeEach(module('wejay'));
+
+  xit('should have tests', inject(function(playlist) {
+
+	  //expect(playlist.doSomething()).to.equal('something');
+
+  }));
+
+});
