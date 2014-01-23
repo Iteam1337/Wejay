7d1ad7487c1472daa24501a6a4f935d27703614a
irony <cl@iteam.se>
2014-01-23 11:48:51 +0100

    remove logic for next song temporarily

--- directive/playlist/playlist.js
+++ directive/playlist/playlist.js
@@ -7,7 +7,19 @@ angular.module('wejay').directive('playlist', function($rootScope, playlist, spo
     replace: true,
     templateUrl: 'directive/playlist/playlist.html',
     link: function(scope, element, attrs, fn) {
-      
+
+
+      // var dropBoxDropEventListener = function (e) {
+      //     if (e.preventDefault) {
+      //       e.preventDefault();
+      //     }
+      //     var droppedUri = e.dataTransfer.getData('text');
+      //     console.log('drop', arguments);
+      // };
+
+      // $(element).addEventListener('drop', dropBoxDropEventListener, false);
+
+
     }
   };
 });
--- partial/room/room.js
+++ partial/room/room.js
@@ -56,23 +56,23 @@ angular.module('wejay').controller('RoomCtrl',function(socket, $rootScope, $scop
     player.addEventListener('change', function (p) {
       console.log('change', p);
 
-      // next
-      if (!p.data.playing || p.data.position === 0)
-      {
-        if($scope.master && p.data.track.uri === $scope.nowPlaying.spotifyId){
-          socket.emit('skip', $scope.nowPlaying);
-        } 
-      } else {
-        // new song
-        if (p.data.track && !p.data.track.adversiment && $scope.nowPlaying){
-          if (p.data.track.uri !== $scope.nowPlaying.spotifyId){
-            $scope.master = false;
-          } else {
-            $scope.master = true;
-          }
-          $scope.safeApply();
-        }
-      }
+      // // next
+      // if (!p.data.playing)
+      // {
+      //   if($scope.master && p.data.track.uri === $scope.nowPlaying.spotifyId){
+      //     socket.emit('skip', $scope.nowPlaying);
+      //   } 
+      // } else {
+      //   // new song
+      //   if (p.data.track && !p.data.track.adversiment && $scope.nowPlaying){
+      //     if (p.data.track.uri !== $scope.nowPlaying.spotifyId){
+      //       $scope.master = false;
+      //     } else {
+      //       $scope.master = true;
+      //     }
+      //     $scope.safeApply();
+      //   }
+      // }
 
     });
 
