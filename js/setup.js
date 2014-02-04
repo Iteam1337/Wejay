0edd8e7bac67436cdfc717251c20203d8de0406d
irony <cl@iteam.se>
2014-02-04 11:41:18 +0100

    at least no hint fails

--- js/setup.js
+++ js/setup.js
@@ -7,23 +7,24 @@ angular.module('wejay').config(function ($compileProvider, $provide) {
   $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|spotify):/);
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|spotify):/);
 
+
+  require(['$api/auth#Auth', '$api/audio', '$api/models','$api/toplists#Toplist', '$views/image#Image','$api/library#Library', '$api/facebook'], function (Auth, audio, models, toplist, image, library, facebook) {
+    var spotify = {};
+    spotify.auth    = new Auth();
+    spotify.audio   = audio;
+    spotify.models  = models;
+    spotify.toplist = toplist;
+    spotify.image   = image;
+    spotify.library = library;
+    spotify.facebook = facebook;
+    
+    $provide.provider('spotify', function() {
+      return spotify;
+    });
+    angular.bootstrap(document, ['wejay']); // start now
+  });
 });
 
-require(['$api/auth#Auth', '$api/audio', '$api/models','$api/toplists#Toplist', '$views/image#Image','$api/library#Library', '$api/facebook'], function (Auth, audio, models, toplist, image, library, facebook) {
-  var spotify = {};
-  spotify.auth    = new Auth();
-  spotify.audio   = audio;
-  spotify.models  = models;
-  spotify.toplist = toplist;
-  spotify.image   = image;
-  spotify.library = library;
-  spotify.facebook = facebook;
-  
-  $provide.provider('spotify', function() {
-    return spotify;
-  });
-  angular.bootstrap(document, ['wejay']); // start now
-});
 
 angular.module('wejay').run(function($rootScope) {
 
